80b41ebe32d0dd1e8ff9768f0a4f5c4cfe6cbbbf
Yushan Wang <yushanwang0816@gmail.com>
2023-10-03 18:54:31 +0800

    work filter done

---
+++ js/filter.js
@@ -0,0 +1,21 @@
+const filterContainer = document.querySelector(".work-filter");
+workItems = document.querySelectorAll(".work-item");
+
+filterContainer.addEventListener("click", (event) => {
+	if(event.target.classList.contains("filter-item")) {
+		filterContainer.querySelector(".active").classList.remove("active");
+
+		event.target.classList.add("active");
+		const filterValue = event.target.getAttribute("data-filter");
+		workItems.forEach((item) => {
+			if (item.classList.contains(filterValue) || filterValue === 'all') {
+				item.classList.remove("hide");
+				item.classList.add("show");
+			}
+			else {
+				item.classList.remove("show");
+				item.classList.add("hide");
+			}
+		})
+	}
+})
